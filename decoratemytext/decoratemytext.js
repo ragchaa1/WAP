11ca0437b8845cb2b3848f7713bc1f0c128f4f46
ragchaa <ragchaa1@gmail.com>
2019-06-04 14:16:12 -0500

    ascii

--- decoratemytext/decoratemytext.js
+++ decoratemytext/decoratemytext.js
@@ -1,17 +1,30 @@
-
-function hi(){
+function hi() {
     alert('Hello world');
 }
 
-function changeText(){
- document.getElementById("tar").style.fontSize=24+"px";
+var timer;
+
+
+function changeText() {
+    area = document.getElementById('tar');
+    if (timer) {
+        clearInterval(timer);
+        timer = null;
+
+    } else {
+        timer = setInterval(function () {
+            var fontSize = window.getComputedStyle(area, null).getPropertyValue("font-size");
+            fontSize = parseInt(fontSize) + 2 + "px"
+            area.style.fontSize = fontSize;
+        }, 500);
+    }
+    //   document.getElementById("tar").style.fontSize = 24 + "px";
 }
 
-function changeCheck(value){
-    if(value.checked) {
+function changeCheck(value) {
+    if (value.checked) {
         document.getElementById("tar").style.fontWeight = "bold";
-    }
-    else {
+    } else {
         document.getElementById("tar").style.fontWeight = "normal";
     }
 }
