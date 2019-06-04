a2b36c0faa4dc876205ce99186d8f47e2fa8e726
ragchaa <ragchaa1@gmail.com>
2019-06-04 14:44:49 -0500

    ascii

--- decoratemytext/decoratemytext.js
+++ decoratemytext/decoratemytext.js
@@ -6,16 +6,20 @@ var timer;
 
 
 function changeText() {
+
     area = document.getElementById('tar');
+
     if (timer) {
         clearInterval(timer);
-        timer = null;
-
+        console.log('if timer = = = '+timer);
     } else {
         timer = setInterval(function () {
             var fontSize = window.getComputedStyle(area, null).getPropertyValue("font-size");
             fontSize = parseInt(fontSize) + 2 + "px"
             area.style.fontSize = fontSize;
+
+            console.log('else timer = = = '+timer);
+
         }, 500);
     }
     //   document.getElementById("tar").style.fontSize = 24 + "px";
